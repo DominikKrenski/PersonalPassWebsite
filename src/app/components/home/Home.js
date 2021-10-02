b87aad7366aa426c3331b35a8fe94d09887f8d3b
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-02 20:37:42 +0200

    Home component added

---
+++ src/app/components/home/Home.js
@@ -0,0 +1,13 @@
+import HomeNavigation from './home-navigation/HomeNavigation';
+
+import './Home.local.scss';
+
+const Home = () => {
+  return (
+    <div id="home">
+      <HomeNavigation />
+    </div>
+  )
+}
+
+export default Home;
