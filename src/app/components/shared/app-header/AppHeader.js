b15f132008ce364e7d88082702c35a89c8286d2f
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-04 14:59:46 +0200

    AppHeader component positioned

---
+++ src/app/components/shared/app-header/AppHeader.js
@@ -0,0 +1,11 @@
+import './AppHeader.local.scss';
+
+const AppHeader = () => {
+  return (
+    <div id="app-header" className="column is-full">
+      <p>Personal<span>Pass</span></p>
+    </div>
+  )
+}
+
+export default AppHeader;
