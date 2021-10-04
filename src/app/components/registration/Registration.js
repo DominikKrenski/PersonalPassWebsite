76b3e58754fc4b148f76daebd1a932ef7a11b894
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-04 15:00:20 +0200

    Required components added to Registration

--- src/app/components/registration/Registration.js
+++ src/app/components/registration/Registration.js
@@ -1,11 +1,15 @@
 import RegistrationForm from './registration-form/RegistrationForm';
+import AppHeader from '../shared/app-header/AppHeader';
+import AppFooter from '../shared/app-footer/AppFooter';
 
 import './Registration.local.scss';
 
 const Registration = () => {
   return (
-    <div id="registration" className="columns">
+    <div id="registration" className="columns is-multiline">
+      <AppHeader />
       <RegistrationForm />
+      <AppFooter />
     </div>
   )
 }
