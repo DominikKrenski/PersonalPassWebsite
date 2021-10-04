f05df9379a188a6319073f7b8d658740f378da9e
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-04 10:44:16 +0200

    Navigation for mobile devices positioned

--- src/app/components/home/home-navigation/HomeNavigation.js
+++ src/app/components/home/home-navigation/HomeNavigation.js
@@ -1,20 +1,41 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './HomeNavigation.local.scss';
 
 const HomeNavigation = () => {
+  const [burgerActive, toggleBurgerActive] = useState(false);
+
+  const handleBurgerClick = () => {
+    if (burgerActive) {
+      toggleBurgerActive(false);
+    } else {
+      toggleBurgerActive(true);
+    }
+  }
+
   return (
     <nav id="home-nav" className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">Personal<span>Pass</span></Link>
-        <Link to="#" role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="homeNavigation">
+        <Link to="#"
+          role="button"
+          className={`navbar-burger ${burgerActive ? 'is-active' : ''}`}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="homeNavigation"
+          onClick={handleBurgerClick}
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </Link>
       </div>
 
-      <div id="homeNavigation" className="navbar-menu">
+      <div
+        id="homeNavigation"
+        className={`navbar-menu ${burgerActive ? 'is-active' : ''}`}
+      >
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="buttons">
