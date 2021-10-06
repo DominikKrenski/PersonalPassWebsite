9fb8e63c0c300e7d4de9d2218e2c995c010c2707
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-06 18:02:15 +0200

    Tests for displaying registration form added

---
+++ cypress/integration/01.registration-page/01.display.spec.js
@@ -0,0 +1,24 @@
+describe('REGISTRATION PAGE DISPLAY', () => {
+  it('should display registration form', () => {
+    cy.visit('/signup');
+    cy.get('form').should('have.length', 1);
+    cy.get('button').should('have.text', 'Register');
+    cy.get('password-requirements').should('not.exist');
+  });
+
+  it('should display password requirements when password got focus', () => {
+    cy.visit('/signup');
+    cy.get('input[type="password"]').first().focus();
+    cy.get('#password-requirements').should('have.length', 1);
+  });
+
+  it('should change password field after clicking on eye icon', () => {
+    cy.visit('/signup');
+    cy.get('input[name="password"]').as('passwordField');
+    cy.get('@passwordField').should('have.attr', 'type', 'password');
+    cy.get('#eye-icon').click();
+    cy.get('@passwordField').should('have.attr', 'type', 'text');
+    cy.get('#eye-icon').click();
+    cy.get('@passwordField').should('have.attr', 'type', 'password');
+  });
+})
