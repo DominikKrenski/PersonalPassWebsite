fc5d732d45290ca30e9051dc1a21ba86a4f2065e
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-11 16:51:21 +0200

    Login component positioned

--- src/app/components/login/Login.js
+++ src/app/components/login/Login.js
@@ -1,9 +1,15 @@
+import LoginForm from './login-form/LoginForm';
+import AppHeader from '../shared/app-header/AppHeader';
+import AppFooter from '../shared/app-footer/AppFooter';
+
 import './Login.local.scss';
 
 const Login = () => {
   return (
-    <div id="login">
-      <p>Login Component</p>
+    <div id="login" className="columns is-multiline">
+      <AppHeader />
+      <LoginForm />
+      <AppFooter />
     </div>
   )
 }
