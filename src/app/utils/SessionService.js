ffa927a1e9d8c3615ee2660f4d17d3ab1eeea6d9
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-12 20:25:34 +0100

    Documentation for SessionService added

--- src/app/utils/SessionService.js
+++ src/app/utils/SessionService.js
@@ -1,20 +1,50 @@
+/**
+ * Contains logic responsible for handling SessionStorage
+ * related tasks.
+ *
+ * @module SessionService
+ */
+
+/**
+ * Class that encapsulates logic for handling SessionStorage related tasks.
+ */
 class SessionService {
-  #storage = window.sessionStorage;
+  #storage = window.sessionStorage; // sessionStorage handler
 
   constructor() {}
 
+  /**
+   * Sets new value in Session Storage
+   *
+   * @param {string} key the name of the key
+   * @param {string} value  the value to be stored
+   */
   set(key, value) {
     this.#storage.setItem(key, value);
   }
 
+  /**
+   * Retrieves data stored at given key.
+   *
+   * @param {string} key the name of the key
+   * @returns {(string|null)} null if given key does not exists; value otherwise
+   */
   get(key) {
     return this.#storage.getItem(key);
   }
 
+  /**
+   * The name of the key that sould be removed
+   *
+   * @param {string} key the name of the key
+   */
   remove(key) {
     this.#storage.removeItem(key);
   }
 
+  /**
+   * Removes all items from Session Storage
+   */
   clear() {
     this.#storage.clear();
   }
