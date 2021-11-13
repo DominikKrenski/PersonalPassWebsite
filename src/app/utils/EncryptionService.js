2b3ed8d6d42738ee2aff1aabf4e42113d1920847
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-13 21:48:59 +0100

    Documentation added to EncryptionService

--- src/app/utils/EncryptionService.js
+++ src/app/utils/EncryptionService.js
@@ -1,9 +1,74 @@
+/**
+ * Module handling all encryption related tasks
+ *
+ * @module EncryptionService
+ */
+
+/**
+ * Object defining result of master key encryption
+ *
+ * @typedef {Object} MasterKeyEncryption
+ * @property {string} vector - initialization vector in HEX format
+ * @property {string} masterKey - encrypted master key in HEX format
+ */
+
+/**
+ * Object defining result of data encryption
+ *
+ * @typedef {Object} DataEncryption
+ * @property {string} vector - initialization vector in HEX format
+ * @property {string} encryptedData - encrypted data in HEX format
+ */
+
+/**
+ * Object describing registration data
+ *
+ * @typedef {Object} InputRegistrationData
+ * @property {string} email - user's email
+ * @property {string} password - user's password
+ * @property {string} reminder - password reminder
+ */
+
+/**
+ * Object describing prepared registration data
+ *
+ * @typedef {Object} OutputRegistrationData
+ * @property {string} email - user's email
+ * @property {string} password - double hashed user's password in HEX format
+ * @property {string} salt - salt used to derive bits from password in HEX format
+ * @property {string} reminder - password reminder
+ */
+
+/**
+ * Object describing prepared login data
+ *
+ * @typedef {Object} LoginData
+ * @property {string} email - user's email
+ * @property {string} password - double hashed derivation key generated from user's password
+ */
+
+/**
+ * Class containing methods for encrypting/decrypting data
+ */
 class EncryptionService {
+
+  /**
+   * Converts string into Uint8Array
+   *
+   * @param {string} data data to be converted
+   * @returns {Uint8Array} array representation of input string
+   */
   #convertStringIntoUint8Array(data) {
     const encoder = new TextEncoder();
     return encoder.encode(data);
   }
 
+  /**
+   * Converts Uint8Array to HEX string
+   *
+   * @param {Uint8Array} input array of bytes
+   * @returns {string} HEX-encoded string
+   */
   #convertTypedArrayIntoHex(input) {
     const arr = new Uint8Array(input);
     const tmp = [];
@@ -15,6 +80,33 @@ class EncryptionService {
     return tmp.join('');
   }
 
+  /**
+   * Checks if given string is valid HEX
+   *
+   * @param {string} hex HEX string to validate
+   * @returns {boolean} true if HEX is valid; false otherwise
+   */
+  #checkHexString(hex) {
+    let flag = true;
+
+    if (hex.length % 2 !== 0) {
+      flag = false;
+    }
+
+    if (!/[0-9A-F]/i.test(hex)) {
+      flag = false;
+    }
+
+    return flag;
+  }
+
+  /**
+   * Converts HEX-encoded string to Uint8Array
+   *
+   * @param {string} hex HEX-encoded string
+   * @returns {Uint8Array} array representation of given HEX-encoded string
+   * @throws Throws an error if data is not valid HEX string
+   */
   #convertHexIntoTypedArray(hex) {
     if (hex.length % 2 !== 0) {
       throw new Error('HEX string has invalid number of characters');
@@ -33,11 +125,25 @@ class EncryptionService {
     return new Uint8Array(ints);
   }
 
+  /**
+   * Generates Uint8Array given length
+   *
+   * @param {number} length number of bytes to generate
+   * @returns {Uint8Array} array filled with random bytes
+   */
   #generateRandomValues(length) {
     const values = new Uint8Array(length);
     return window.crypto.getRandomValues(values);
   }
 
+  /**
+   * Converts UTF-16 string into CryptoKey that can be used to generate derivatation key with PBKDF2 algorithm.
+   *
+   * @param {string} str string to be coverted into CryptoKey
+   * @returns {Promise} Promise object representing CryptoKey
+   * @throws {SyntaxError} raised when `keyUsages is empty but the unwrapped key is of type `secret` or `private`
+   * @throws {TypeError} raised when trying to use an invalid format or if the `keyData` is not suited for that format
+   */
   async #importPbkdfKey(str) {
     const keyData = this.#convertStringIntoUint8Array(str);
 
@@ -50,6 +156,14 @@ class EncryptionService {
     );
   }
 
+  /**
+   * Converts Uint8Array into CryptoKey that can be used to encrypt/decrypt text with AES-GCM algorithm
+   *
+   * @param {Uint8Array} keyBytes key stored as raw byte array
+   * @returns {Promise} Promise object representing CryptoKey
+   * @throws {SyntaxError} raised when `keyUsages` is empty but the unwrapped key is of type `secret` or `private`
+   * @throws {TypeError} raised when trying to use an invalid format or if the `keyData` is not suited for that format
+   */
   async #importAesKey(keyBytes) {
     return window.crypto.subtle.importKey(
       'raw',
@@ -62,6 +176,13 @@ class EncryptionService {
     );
   }
 
+  /**
+   * Derives bits from given key.
+   *
+   * @param {string} data Key's string representation
+   * @param {Uint8Array} salt array containing salt used during processing
+   * @returns {Promise} Promise object representing derived bits
+   */
   async #generateDerivationKey(data, salt) {
     const key = await this.#importPbkdfKey(data);
 
@@ -77,12 +198,29 @@ class EncryptionService {
     );
   }
 
+  /**
+   * Computes double SHA-256 hash from given input array
+   *
+   * @param {Uint8Array} derivationKey input array
+   * @returns {Promise} Promise object representing  computed hash
+   */
   async #generateDerivationKeyHash(derivationKey) {
     const firstHash = await window.crypto.subtle.digest('SHA-256', derivationKey);
     const secondHash = await window.crypto.subtle.digest('SHA-256', firstHash);
     return secondHash;
   }
 
+  /**
+   * Encrypt data with AES-GCM SHA-256 algorithm
+   *
+   * @param {string} data text to be encrypted
+   * @param {Uint8Array} masterKeyBytes array of bytes representing encryption key
+   * @returns {Promise} Promise object representing encrypted text {@link DataEncryption}
+   * @throws {InvalidAccessError} raised when the requested operation is not valid for the provided key
+   * (invalid encryption algorithm, or invalid key for the specified encryption algorithm)
+   * @throws {OperationError} raised when operation failed for an operation specific reason (algorithm parameters
+   * of invalid sized, or AES-GCM plaintext logner than 2^39 - 256 bytes)
+   */
   async encryptData(data, masterKeyBytes) {
     const dataUint8Array = this.#convertStringIntoUint8Array(data);
     const vector = this.#generateRandomValues(12);
@@ -103,7 +241,27 @@ class EncryptionService {
     }
   }
 
+  /**
+   * Decrypt AES-GCM data encoded as HEX string
+   *
+   * @param {string} encryptedDataHEX HEX-encoded encrypted data
+   * @param {string} vectorHEX HEX-encoded initialization vector
+   * @param {Uint8Array} masterKeyRaw byte array containing encryption key
+   * @returns {Promise} Promise object representing decrypted text
+   * @throws {InvalidAccessError} raised when the requested operation is not valid for the provided key
+   * (invalid encryption algorithm, or invalid key for the specified encryption algorithm)
+   * @throws {OperationError} raised when operation failed for an operation specific reason (algorithm parameters
+   * of invalid sized, or AES-GCM plaintext logner than 2^39 - 256 bytes)
+   */
   async decryptData(encryptedDataHEX, vectorHEX, masterKeyRaw) {
+    if (!this.#checkHexString(encryptedDataHEX)) {
+      throw new Error('Encrypted data is not valid HEX string');
+    }
+
+    if (!this.#checkHexString(vectorHEX)) {
+      throw new Error('Vector is not valid HEX string');
+    }
+
     const encryptedDataRaw = this.#convertHexIntoTypedArray(encryptedDataHEX);
     const vectorRaw = this.#convertHexIntoTypedArray(vectorHEX);
     const cryptoKey = await this.#importAesKey(masterKeyRaw);
@@ -120,6 +278,17 @@ class EncryptionService {
     return new TextDecoder().decode(decrypted);
   }
 
+  /**
+   * Encrypts master key with AES-GCM SHA-256 algorithm
+   *
+   * @param {Uint8Array} masterKeyBytes byte array representing master key
+   * @param {string} encryptionKeyHEX application's encryption key stored in HEX format
+   * @returns {Promise} Promise object representing encrypted master key {@link MasterKeyEncryption}
+   * @throws {InvalidAccessError} raised when the requested operation is not valid for the provided key
+   * (invalid encryption algorithm, or invalid key for the specified encryption algorithm)
+   * @throws {OperationError} raised when operation failed for an operation specific reason (algorithm parameters
+   * of invalid sized, or AES-GCM plaintext logner than 2^39 - 256 bytes)
+   */
   async encryptMasterKey(masterKeyBytes, encryptionKeyHEX) {
     const encryptionKeyRaw = this.#convertHexIntoTypedArray(encryptionKeyHEX);
     const vector = this.#generateRandomValues(12);
@@ -140,6 +309,18 @@ class EncryptionService {
     }
   }
 
+  /**
+   * Decrypts master key
+   *
+   * @param {string} masterKeyHEX encrypted master key in HEX format
+   * @param {string} encryptionKeyHEX application's encryption key in HEX format
+   * @param {string} vectorHEX application's initialization vector in HEX format
+   * @returns {Promise} Promise object representing dedcrypted master key as Uint8Array
+   * @throws {InvalidAccessError} raised when the requested operation is not valid for the provided key
+   * (invalid encryption algorithm, or invalid key for the specified encryption algorithm)
+   * @throws {OperationError} raised when operation failed for an operation specific reason (algorithm parameters
+   * of invalid sized, or AES-GCM plaintext logner than 2^39 - 256 bytes)
+   */
   async decryptMasterKey(masterKeyHEX, encryptionKeyHEX, vectorHEX) {
     const masterKeyRaw = this.#convertHexIntoTypedArray(masterKeyHEX);
     const encryptionKeyRaw = this.#convertHexIntoTypedArray(encryptionKeyHEX);
@@ -156,7 +337,18 @@ class EncryptionService {
     );
   }
 
+  /**
+   * Regenerates derivation key from password and salt in HEX format
+   *
+   * @param {string} password plain password
+   * @param {string} saltHex salt in HEX format
+   * @returns {Promise} Promise object representing regenerated derivation key
+   */
   async regenerateDerivationKey(password, saltHex) {
+    if (!this.#checkHexString(saltHex)) {
+      throw new Error('Salt is not valid HEX string');
+    }
+
     const salt = this.#convertHexIntoTypedArray(saltHex);
     const key = await this.#importPbkdfKey(password);
 
@@ -172,6 +364,12 @@ class EncryptionService {
     );
   }
 
+  /**
+   * Prepare data for registration process
+   *
+   * @param {InputRegistrationData} data registration data
+   * @returns {OutputRegistrationData} registration data after encoding {@link OutputRegistrationData}
+   */
   async prepareRegistrationData(data) {
     const {email, password, reminder} = data;
 
@@ -192,6 +390,12 @@ class EncryptionService {
     return registrationData;
   }
 
+  /**
+   *
+   * @param {string} email user's email
+   * @param {Uint8Array} derivationKey derivation key
+   * @returns {LoginData} login data after encoding {@link LoginData}
+   */
   async prepareLoginData(email, derivationKey) {
     const derivationKeyHash = await this.#generateDerivationKeyHash(derivationKey);
 
@@ -201,6 +405,12 @@ class EncryptionService {
     }
   }
 
+  /**
+   * Converts Base64-encoded text into UTF-16 text
+   *
+   * @param {string} data Base64-encoded text
+   * @returns {string} UTF-16 text
+   */
   convertBase64ToString(data) {
     return decodeURIComponent(escape(window.atob(data)));
   }
