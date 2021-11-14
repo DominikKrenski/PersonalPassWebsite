173d3351715a126542cc96e02655d5e27d9271aa
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-14 22:10:40 +0100

    Initial implementation of ErrorService added

---
+++ src/app/utils/ErrorService.js
@@ -0,0 +1,58 @@
+/**
+ * Contains logic responsible for passing server errors around whole application
+ *
+ * @module ErrorService
+ */
+
+/**
+ * Object defining server error
+ *
+ * @typedef {Object} ApiError
+ * @property {string} status - error response status
+ * @property {string} timestamp - date when error occurred
+ * @property {string} message - error's description
+ * @property {string[]} validationMessages - optional validation messages
+ */
+
+import { BehaviorSubject, Observable } from 'rxjs';
+
+/**
+ * Class that encapsulates logic responsible for passing errors
+ */
+class ErrorService {
+  #subject
+
+  /**
+   * Constructs ErrorService instance
+   */
+  constructor() {
+    this.#subject = new BehaviorSubject(null);
+  }
+
+  /**
+   * Returns observable to current api error
+   *
+   * @returns {Observable<ApiError>}
+   */
+  getError() {
+    return this.#subject.asObservable();
+  }
+
+  /**
+   * Pass new api error to all subscribers
+   *
+   * @param {ApiError} error error response
+   */
+  updateError(error) {
+    this.#subject.next(error);
+  }
+}
+
+/**
+ * ErrorService instance
+ *
+ * @private
+ */
+const errorService = new ErrorService();
+
+export default errorService;
