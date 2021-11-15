e0583156349dbaa7076d33250df564882f78ddf8
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-15 09:36:44 +0100

    Registration component updated to use ErrorService

--- src/app/components/registration/Registration.js
+++ src/app/components/registration/Registration.js
@@ -1,15 +1,27 @@
+import { useState, useEffect } from 'react';
+
 import RegistrationForm from './registration-form/RegistrationForm';
 import AppHeader from '../shared/app-header/AppHeader';
 import AppFooter from '../shared/app-footer/AppFooter';
 import AppError from '../shared/app-error/AppError';
 
+import errorService from '../../utils/ErrorService';
+
 import './Registration.local.scss';
 
 const Registration = () => {
+  const [apiError, setApiError] = useState(null);
+
+  useEffect(() => {
+    const errorServiceSubscription = errorService.getError().subscribe(err => setApiError(err));
+
+    return () => errorServiceSubscription.unsubscribe();
+  });
+
   return (
     <div id="registration" className="columns is-multiline">
       <AppHeader />
-      <AppError />
+      {apiError && <AppError error={apiError} />}
       <RegistrationForm />
       <AppFooter />
     </div>
