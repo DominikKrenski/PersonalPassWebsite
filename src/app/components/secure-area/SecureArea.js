b2558c826a39f3d7b17753a9ba42631acf4a98ac
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-24 20:36:38 +0100

    SecureNav added to SecureArea

--- src/app/components/secure-area/SecureArea.js
+++ src/app/components/secure-area/SecureArea.js
@@ -3,6 +3,7 @@ import { useIdleTimer } from 'react-idle-timer';
 import timerService from '../../utils/TimerService';
 
 import SecureHeader from '../shared/secure-header/SecureHeader';
+import SecureNav from './secure-nav/SecureNav';
 import AppFooter from '../shared/app-footer/AppFooter';
 
 import './SecureArea.local.scss';
@@ -19,6 +20,7 @@ const SecureArea = () => {
   return (
     <div id="secure-area">
       <SecureHeader />
+      <SecureNav />
       <AppFooter />
     </div>
   )
