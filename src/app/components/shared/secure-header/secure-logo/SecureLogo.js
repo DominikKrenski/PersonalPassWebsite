187f0c4812a97345b27c7896d59018be1dbdb98f
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-24 14:59:14 +0100

    SecureLogo component added

---
+++ src/app/components/shared/secure-header/secure-logo/SecureLogo.js
@@ -0,0 +1,11 @@
+import './SecureLogo.local.scss';
+
+const SecureLogo = () => {
+  return (
+    <div id="secure-logo" className="column is-4">
+      <p>Personal<span>Pass</span></p>
+    </div>
+  )
+}
+
+export default SecureLogo;
