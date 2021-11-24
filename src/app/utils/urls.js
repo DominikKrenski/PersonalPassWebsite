393850484ae16d99812a1a433970cafe1fadb930
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-24 15:02:45 +0100

    Logout route added to urls.js

--- src/app/utils/urls.js
+++ src/app/utils/urls.js
@@ -9,6 +9,7 @@
 const urls = {
   signup: '/auth/signup',
   signin: '/auth/signin',
+  signout: '/auth/signout',
   salt: '/auth/salt',
   accountDetails: '/accounts',
   updateEmail: '/accounts/email',
