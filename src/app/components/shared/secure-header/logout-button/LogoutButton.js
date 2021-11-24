c7ccf699a18d2b59a39ec92170be5e482c6bbee3
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-24 14:59:33 +0100

    Template for LogoutButton component added

---
+++ src/app/components/shared/secure-header/logout-button/LogoutButton.js
@@ -0,0 +1,11 @@
+import './LogoutButton.local.scss';
+
+const LogoutButton = () => {
+  return (
+    <div id="logout-button" className="column is-4">
+      <p>Logout Button</p>
+    </div>
+  )
+}
+
+export default LogoutButton;
