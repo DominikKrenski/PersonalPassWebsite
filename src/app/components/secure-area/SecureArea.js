169dd54e32432e911047683b293b8a06cdab54d3
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-24 15:24:15 +0100

    AppFooter added to SecureArea

--- src/app/components/secure-area/SecureArea.js
+++ src/app/components/secure-area/SecureArea.js
@@ -3,6 +3,7 @@ import { useIdleTimer } from 'react-idle-timer';
 import timerService from '../../utils/TimerService';
 
 import SecureHeader from '../shared/secure-header/SecureHeader';
+import AppFooter from '../shared/app-footer/AppFooter';
 
 import './SecureArea.local.scss';
 
@@ -18,6 +19,7 @@ const SecureArea = () => {
   return (
     <div id="secure-area">
       <SecureHeader />
+      <AppFooter />
     </div>
   )
 }
