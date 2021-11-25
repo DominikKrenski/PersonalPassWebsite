d5c5623d61f633fdb46ee58cfa8d96dc1be6104e
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-25 10:45:07 +0100

    Initial implementation of Account component added

---
+++ src/app/components/secure-area/account/Account.js
@@ -0,0 +1,11 @@
+import './Account.local.scss';
+
+const Account = () => {
+  return (
+    <div id="account-details" className="column is-9">
+      <p>Account Component</p>
+    </div>
+  )
+}
+
+export default Account;
