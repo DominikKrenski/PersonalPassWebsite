de327954ca1dc1c61edc89aeca9d558d4027fead
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-25 10:44:40 +0100

    Dummy implementation of Items component added

---
+++ src/app/components/secure-area/items/Items.js
@@ -0,0 +1,11 @@
+import './Items.local.scss';
+
+const Items = () => {
+  return (
+    <div id="all-items" className="column is-9">
+      <p>All Items Component</p>
+    </div>
+  )
+}
+
+export default Items;
