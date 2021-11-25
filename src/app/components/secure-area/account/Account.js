0b4e3bde9de565b4549d439dac3eaabb7b5e7857
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-25 17:16:50 +0100

    Template for LoginCredentials table added

--- src/app/components/secure-area/account/Account.js
+++ src/app/components/secure-area/account/Account.js
@@ -3,7 +3,29 @@ import './Account.local.scss';
 const Account = () => {
   return (
     <div id="account-details" className="column is-10">
-      <p>Account Component</p>
+      <h1>Account Settings</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="2">Login Credentials</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Account Email</td>
+            <td>bla bla bla safaskdfaklfjakljfaklfjalkjfklfjlakjfklajflakfjalkjalkfjalkfjalsdkfjaljflajfalkfjl</td>
+          </tr>
+          <tr>
+            <td>Master Password</td>
+            <td>bla bla bla</td>
+          </tr>
+          <tr>
+            <td>Master Password Reminder</td>
+            <td>bla bla bla</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
