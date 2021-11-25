9c6e61ad11c021a0fa8e06c69ad4bb6cc8d85463
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-25 10:46:26 +0100

    New components added to SecureArea

--- src/app/components/secure-area/SecureArea.js
+++ src/app/components/secure-area/SecureArea.js
@@ -1,3 +1,4 @@
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import { useIdleTimer } from 'react-idle-timer';
 
 import timerService from '../../utils/TimerService';
@@ -5,10 +6,14 @@ import timerService from '../../utils/TimerService';
 import SecureHeader from '../shared/secure-header/SecureHeader';
 import SecureNav from './secure-nav/SecureNav';
 import AppFooter from '../shared/app-footer/AppFooter';
+import Items from './items/Items';
+import Account from './account/Account';
 
 import './SecureArea.local.scss';
 
 const SecureArea = () => {
+  const { path } = useRouteMatch();
+
   const handleOnAction = event => {
     timerService.activityDetected(true);
   }
@@ -20,7 +25,17 @@ const SecureArea = () => {
   return (
     <div id="secure-area">
       <SecureHeader />
+      <div id="content-area" className="columns">
       <SecureNav />
+        <Switch>
+          <Route exact path={path}>
+            <Items />
+          </Route>
+          <Route path={`${path}/account`}>
+            <Account />
+          </Route>
+        </Switch>
+      </div>
       <AppFooter />
     </div>
   )
