3299c689a86499d1cb1b2d334059e80561392e65
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-30 16:29:38 +0100

    Translations added to HomeBanner

--- src/app/components/home/home-banner/HomeBanner.js
+++ src/app/components/home/home-banner/HomeBanner.js
@@ -1,11 +1,15 @@
+import { useTranslation } from 'react-i18next';
+
 import LockIcon from '../../../../assets/img/lock-64.png';
 import './HomeBanner.local.scss';
 
 const HomeBanner = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="home-banner" className="hero">
       <div className="hero-body">
-        <p className="title">Store your data securely</p>
+        <p className="title">{t('homeBanner.title')}</p>
       </div>
       <figure id="lock-icon" className="image is-64x64">
         <img src={LockIcon} alt="lock icon"/>
