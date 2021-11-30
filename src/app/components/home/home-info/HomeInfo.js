9746c2ddcacc951ee32ecdfa04f213e40ea59bba
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-30 16:29:57 +0100

    Translations added to HomeInfo

--- src/app/components/home/home-info/HomeInfo.js
+++ src/app/components/home/home-info/HomeInfo.js
@@ -1,53 +1,45 @@
+import { useTranslation } from 'react-i18next';
+
 import './HomeInfo.local.scss';
 
 const HomeInfo = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="home-info" className="columns is-multiline">
       <div className="column is-two-fifhts">
         <div className="info-title">
-          <p>Strong algorithms</p>
+          <p>{t('homeInfo.algorithm.title')}</p>
         </div>
         <div className="info-text">
-          <p>
-            Personal Pass implements AES-256 bit encryption with PBKDF2 SHA-256 and salted hashes to ensure
-            complete security in the web. You'll create a password mmanager account with an email address and a strong master
-            password to locally generate a unique encryption key.
-          </p>
+          <p>{t('homeInfo.algorithm.text')}</p>
         </div>
       </div>
 
       <div className="column is-two-fifths is-offset-one-fifth">
         <div className="info-title">
-          <p>Local-only encryption</p>
+          <p>{t('homeInfo.encryption.title')}</p>
         </div>
         <div className="info-text">
-          <p>
-            Your data is encrypted and decrypted at the device level. Data stored on our servers is kept secret, even from Personal Pass.
-            Your master password, and the keys used to encrypt and decrypt data, are never sent to our servers.
-          </p>
+          <p>{t('homeInfo.encryption.text')}</p>
         </div>
       </div>
 
       <div className="column is-two-fifths">
         <div className="info-title">
-          <p>Open Source</p>
+          <p>{t('homeInfo.openSource.title')}</p>
         </div>
         <div className="info-text">
-          <p>
-            Personal pass is an open source project so anyone can verify the code that protects your data.
-          </p>
+          <p>{t('homeInfo.openSource.text')}</p>
         </div>
       </div>
 
       <div className="column is-two-fifhts is-offset-one-fifth">
         <div className="info-title">
-          <p>On Any Device</p>
+          <p>{t('homeInfo.anyDevice.title')}</p>
         </div>
         <div className="info-text">
-          <p>
-            Our dedicated applications for Android and desktops ley you store data everywhere.
-            Our fast web client apps make sure data encryption is a pleasant experience.
-          </p>
+          <p>{t('homeInfo.anyDevice.text')}</p>
         </div>
       </div>
     </section>
