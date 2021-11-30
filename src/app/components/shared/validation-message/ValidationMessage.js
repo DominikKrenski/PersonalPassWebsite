ba13d7429b1adc8f0c9c067d51b2c942dec16a7b
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-30 21:16:38 +0100

    ValidationMessages component updated to use internationalization

--- src/app/components/shared/validation-message/ValidationMessage.js
+++ src/app/components/shared/validation-message/ValidationMessage.js
@@ -1,46 +1,60 @@
 import PropTypes from 'prop-types';
 
+import i18n from '../../../i18n';
+
 import './ValidationMessage.local.scss';
 
 const ValidationMessage = props => {
-  const { field, errors } = props;
+  const { errors } = props;
   const el = [];
-
-  const capitalize = str => {
-    const arr = str.split('');
-    arr[0] = arr[0].toLocaleUpperCase();
-    return arr.join('');
-  }
 
   Object.keys(errors).forEach(key => {
     switch(key) {
       case 'required':
-        el.push(<li key={key}>{errors[key]}</li>)
+        el.push(<li key={key}>{i18n.language === 'pl' ? 'Pole jest wymagane' : 'Field is required'}</li>);
         break;
       case 'email':
-        el.push(<li key={key}>{errors[key]}</li>);
+        el.push(<li key={key}>{i18n.language === 'pl' ? 'Email nie jest poprawny' : 'Email is not valid'}</li>)
         break;
       case 'minLength':
-        el.push(<li key={key}>{capitalize(field)} must be at least {errors[key]} characters long</li>);
+        el.push(<li key={key}>
+          {i18n.language === 'pl' ? `Pole musi zawierać co najmniej ${errors[key]} znaków` : `Field must be at least ${errors[key]} characters long`};
+        </li>
+        )
         break;
       case 'maxLength':
-        el.push(<li key={key}>{capitalize(field)} must be at most {errors[key]} characters long</li>);
+        el.push(<li key={key}>
+          {i18n.language === 'pl' ? `Pole musi zawierać najwyżej ${errors[key]} znaków` : `Field must be at most ${errors[key]} characters long`};
+        </li>
+        )
         break;
       case 'atLeastOneDigit':
-        el.push(<li key={key}>{capitalize(field)} must contain at least one digit</li>);
+        el.push(<li key={key}>
+          {i18n.language === 'pl' ? `Pole musi zawierać przynajmniej jedną cyfrę` : `Field must contain at least one digit`};
+        </li>
+        )
         break;
       case 'atLeastOneLowercase':
-        el.push(<li key={key}>{capitalize(field)} must contain at least one lowercase letter</li>);
+        el.push(<li key={key}>
+          {i18n.language === 'pl' ? `Pole musi zawierać przynajmniej jedną małą literę` : `Field must contain at least one lowercase letter`};
+        </li>
+        )
         break;
       case 'atLeastOneUppercase':
-        el.push(<li key={key}>{capitalize(field)} must contain at least one uppercase letter</li>);
+        el.push(<li key={key}>
+          {i18n.language === 'pl' ? `Pole musi zawierać przynajmniej jedną wielką literę` : `Field must contain at least one uppercase letter`}
+        </li>
+        )
         break;
       case 'notEmail':
-        el.push(<li key={key}>{capitalize(field)} must not contain email</li>);
+        el.push(<li key={key}>
+          {i18n.language === 'pl' ? `Pole nie może zawierać adresu email` : `Field must not contain email`}
+        </li>
+        )
         break;
       case 'equalField':
-        el.push(<li key={key}>{errors[key]}</li>);
-        break
+        el.push(<li key={key}>{i18n.language === 'pl' ? 'Pola nie są jednakowe' : 'Fields are not equal'}</li>)
+        break;
     }
   });
 
