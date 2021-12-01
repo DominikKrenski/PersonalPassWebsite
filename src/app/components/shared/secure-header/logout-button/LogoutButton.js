84a83a32b4f0d546ece8a1bf96c679d5d7ddd43b
Dominik Kreński <dominik.krenski@gmail.com>
2021-12-01 13:02:19 +0100

    Translation for LogoubButton added

--- src/app/components/shared/secure-header/logout-button/LogoutButton.js
+++ src/app/components/shared/secure-header/logout-button/LogoutButton.js
@@ -1,9 +1,13 @@
+import { useTranslation } from 'react-i18next';
+
 import './LogoutButton.local.scss';
 
 const LogoutButton = () => {
+  const { t } = useTranslation();
+
   return (
     <div id="logout-button-wrapper" className="column is-4">
-      <button id="logout-button" className="button is-rounded">SIGN OUT</button>
+      <button id="logout-button" className="button is-rounded">{t('secureArea.logoutButton')}</button>
     </div>
   )
 }
