b50fa6bab39c9ec52e1925beabaa87f2028c3257
Dominik Kreński <dominik.krenski@gmail.com>
2021-12-01 16:10:48 +0100

    DateService.displayDate() method added

--- src/app/utils/DateService.js
+++ src/app/utils/DateService.js
@@ -6,8 +6,12 @@
 
 import * as dayjs from 'dayjs';
 import * as utc from 'dayjs/plugin/utc';
+import * as timezone from 'dayjs/plugin/timezone';
+import * as customParseFormat from 'dayjs/plugin/customParseFormat';
 
 dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
 
 /**
  * Class that encapsulates logic related to handling dates
@@ -22,6 +26,17 @@ class DateService {
    */
   getTimestamp() {
     return dayjs().utc().format('D/M/YYYY[T]HH:mm:ss.SSS[Z]');
+  }
+
+  /**
+   * Converts date to given time zone
+   *
+   * @param {string} date current date
+   * @param {string} timezone timezone identifier
+   * @returns {string} date transformed to given time zone
+   */
+  displayDate(date, timezone) {
+    return dayjs(date, 'D/M/YYYY[T]HH:mm:ss.SSS[Z]').tz(timezone).format('D/M/YYYY HH:mm:ss');
   }
 }
 
