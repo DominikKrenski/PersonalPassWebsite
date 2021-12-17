7bbc147c4898789d9ac9c07a2e8ac1ba4ac05edf
Dominik Kreński <dominik.krenski@gmail.com>
2021-12-17 12:47:15 +0100

    Confirmation component added

---
+++ src/app/components/shared/confirmation/Confirmation.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+
+import './Confirmation.local.scss';
+
+const Confirmation = props => {
+  const {
+    msg,
+    cancelButtonText,
+    confirmButtonText,
+    cancelCallback,
+    confirmCallback
+  } = props;
+
+  return (
+    <div id="confirmation-wrapper">
+      <div id="confirmation" className="column is-half is-offset-one-quarter">
+        <div id="confirmation-header">
+          <h1>Warning</h1>
+        </div>
+        <div id="confirmation-body">
+          {msg}
+        </div>
+        <div id="confirmation-footer">
+          <button className="button" onClick={cancelCallback}>{cancelButtonText}</button>
+          <button className="button is-danger" onClick={confirmCallback}>{confirmButtonText}</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Confirmation.propTypes = {
+  msg: PropTypes.string.isRequired,
+  cancelButtonText: PropTypes.string.isRequired,
+  confirmButtonText: PropTypes.string.isRequired,
+  cancelCallback: PropTypes.func.isRequired,
+  confirmCallback: PropTypes.func.isRequired
+}
+
+export default Confirmation;
