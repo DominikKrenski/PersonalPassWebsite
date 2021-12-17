dd8e3dbacba3235da13a1ccf9adb2f37e3aed316
Dominik Kreński <dominik.krenski@gmail.com>
2021-12-17 12:47:37 +0100

    Prop types added to DataRow

--- src/app/components/shared/data-table/DataTable.js
+++ src/app/components/shared/data-table/DataTable.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import DataRow from './data-row/DataRow';
 
 import './DataTable.local.scss';
@@ -34,6 +36,36 @@ const DataTable = props => {
       </table>
     </div>
   )
+}
+
+DataTable.propTypes = {
+  showButtonClick: PropTypes.func.isRequired,
+  editButtonClick: PropTypes.func.isRequired,
+  deleteButtonClick: PropTypes.func.isRequired,
+  arr: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+      updatedAt: PropTypes.string.isRequired,
+      entry: PropTypes.exact({
+        entryTitle: PropTypes.string.isRequired,
+        firstName: PropTypes.string,
+        middleName: PropTypes.string,
+        lastName: PropTypes.string,
+        birthday: PropTypes.string,
+        company: PropTypes.string,
+        addressOne: PropTypes.string,
+        addressTwo: PropTypes.string,
+        city: PropTypes.string,
+        country: PropTypes.string,
+        state: PropTypes.string,
+        email: PropTypes.string,
+        phone: PropTypes.string,
+        mobilePhone: PropTypes.string,
+        notes: PropTypes.string
+      })
+    })
+  ).isRequired
 }
 
 export default DataTable;
